ddd4218b4c6a63c873f779c124d608e4e3176a31
Blutronics <toanbum2000@gmail.com>
2020-02-21 19:22:16 -0600

    Finishing up

--- app/src/main/java/edu/lawrence/androiddaycare/RegistrationActivity.java
+++ app/src/main/java/edu/lawrence/androiddaycare/RegistrationActivity.java
@@ -20,7 +20,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class RegistrationActivity extends AppCompatActivity {
-    String id, position, idchi;
+    String position, idchi;
     String sm, sd, sy, em, ed, ey;
     private int selected_provider = -1;
     private Provider[] p = null;
@@ -28,6 +28,7 @@ public class RegistrationActivity extends AppCompatActivity {
     public final static String USER_ID = "edu.lawrence.AndroidDaycare.LoginActivity.USER_ID";
     public final static String PARENT_ID = "edu.lawrence.AndroidDaycare.ParentActivity.PARENT_ID";
     private int selected_p = -1;
+    private String userID;
     private Gson gson;
 
 
@@ -35,11 +36,11 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null) {
-            id = savedInstanceState.getString(LoginActivity.USER_ID);
+            userID = savedInstanceState.getString(LoginActivity.USER_ID);
             idchi = savedInstanceState.getString(ChildrenDashboard.CHILDREN_ID);
         } else {
             Intent intent = getIntent();
-            id = intent.getStringExtra(LoginActivity.USER_ID);
+            userID = intent.getStringExtra(LoginActivity.USER_ID);
             idchi = intent.getStringExtra(ChildrenDashboard.CHILDREN_ID);
         }
         setContentView(R.layout.activity_registration);
@@ -50,10 +51,11 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle arg0) {
         super.onSaveInstanceState(arg0);
-        arg0.putString(LoginActivity.USER_ID, id);
+        arg0.putString(LoginActivity.USER_ID, userID);
     }
 
-    public void search(View view) throws ParseException {
+    private void register(View view) throws ParseException {
+        Provider selected = p[selected_provider];
         EditText Start = findViewById(R.id.start);
         String s = Start.getText().toString();
         EditText End = findViewById(R.id.end);
@@ -63,14 +65,65 @@ public class RegistrationActivity extends AppCompatActivity {
         sd = separated[1];
         sy = separated[2];
         String startdate = sy + "-" + sm + "-" + sd;
-        Date start = new SimpleDateFormat("yyyy-MM-dd").parse(startdate);
+        Date start=new SimpleDateFormat("yyyy-MM-dd").parse(startdate);
 
         String[] eee = e.split("/");
         em = eee[0];
         ed = eee[1];
         ey = eee[2];
         String enddate = ey + "-" + em + "-" + ed;
-        Date end = new SimpleDateFormat("yyyy-MM-dd").parse(enddate);
+        Date end=new SimpleDateFormat("yyyy-MM-dd").parse(enddate);
+        int status = 1;
+        registration newRegis = new registration();
+        newRegis.setChildID(Integer.parseInt(idchi));
+        newRegis.setProviderID(selected.getId());
+        newRegis.setStatus(status);
+        newRegis.setStart(start);
+        newRegis.setEnd(end);
+        new NewRegistrationTask(newRegis).execute();
+        }
+    private class NewRegistrationTask extends AsyncTask<Void, Void, String> {
+        private String uri;
+        private String toSend;
+
+        NewRegistrationTask(registration r) {
+            uri = "http://" + URIHandler.hostName + "/registration/";
+            this.toSend = gson.toJson(r);
+            finish();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return URIHandler.doPost(uri, toSend);
+        }
+        @Override
+        public void onPostExecute(String result) {
+            if ("0".equals(result)) {
+                userMessage("fail");
+            } else {
+                GoDashboard(result);
+            }
+        }
+    }
+
+
+
+    public void search(View view) {
+        EditText Start = findViewById(R.id.start);
+        String s = Start.getText().toString();
+        EditText End = findViewById(R.id.end);
+        String e = End.getText().toString();
+        String[] separated = s.split("/");
+        sm = separated[0];
+        sd = separated[1];
+        sy = separated[2];
+        String startdate = sy + "-" + sm + "-" + sd;
+
+        String[] eee = e.split("/");
+        em = eee[0];
+        ed = eee[1];
+        ey = eee[2];
+        String enddate = ey + "-" + em + "-" + ed;
         new SearchTask(startdate, enddate).execute();
 
     }
@@ -91,7 +144,7 @@ public class RegistrationActivity extends AppCompatActivity {
         @Override
         public void onPostExecute(String result) {
             loadProviders(result);
-        }
+        }}
 
         private void loadProviders(String json) {
             p = null;
@@ -122,25 +175,22 @@ public class RegistrationActivity extends AppCompatActivity {
                 });
             }
         }
-    }
+
 
 
     ////////////get children info and do math to get minage and maxage after registrated///////////
 
 
-    private void getchildren(String json) {
-        Child[] chi = gson.fromJson(json, Child[].class);//list[]--> server
-        String[] childrenStrs = new String[chi.length];
-        int idchil = Integer.parseInt(idchi);
-        Date birth;
-        for (int n = 0; n != idchil; n++) {
-            Child child = chi[n];
-            birth = child.getBirthday();
-        }
 
-    }
 
     private void userMessage(String message) {
         Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void GoDashboard(String parentID) {
+        Intent intent = new Intent(this, ChildrenDashboard.class);
+        intent.putExtra(LoginActivity.USER_ID, userID);
+        intent.putExtra(ParentActivity.PARENT_ID, parentID);
+        intent.putExtra(ChildrenActivity.CHILDREN_ID, idchi);
+        startActivity(intent);
     }
 }
